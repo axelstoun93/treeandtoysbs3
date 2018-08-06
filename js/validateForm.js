36f6ed1e48bcc5a0c71af69127a932ede7ba5839
Stone <axelstoun@mail.ru>
2018-08-07 00:37:52 +0300

    Добавлены всплывающие окна регистрации входа и восстановления пароля

---
+++ js/validateForm.js
@@ -0,0 +1,89 @@
+(function () {
+validateForm();
+function validateForm() {
+
+    $(document).ready(function () {
+        $("#login-form").validate({
+
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                }
+            },
+
+            messages: {
+                password: {
+                    required: "Укажите пароль",
+                    minlength: "Ваш пароль должен содержать не менее 5 символов"
+                },
+                email: "Укажите корректный email адрес"
+            },
+
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
+
+
+        $("#register-form").validate({
+
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                },
+                password_again: {
+                    required: true,
+                    equalTo: "#password"
+                }
+            },
+
+            messages: {
+                password: {
+                    required: "Укажите пароль",
+                    minlength: "Ваш пароль должен содержать не менее 5 символов"
+                },
+                password_again:
+                    {
+                        required: "Повторите пароль",
+                        equalTo: "Пароли должны совпадать друг с другом"
+                    },
+                email: "Укажите корректный email адрес"
+            },
+
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
+
+
+
+        $("#fagot-form").validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                email: "Укажите корректный email адрес"
+            },
+
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
+
+    });
+
+}})();
+
